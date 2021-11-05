7572889bdb33fb163cd74ff6fe34c3df049416ff
sekiya9311 <sekiya9311@gmail.com>
2021-11-05 20:24:56 +0900

    initial commit

---
+++ MinecraftServerManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinecraftServerManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
